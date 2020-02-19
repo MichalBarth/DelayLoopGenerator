7a854d6a9f0ab8a04f4adb60dd4dead0049df2c4
Michal Barth <u17.401@pslib.cloud>
2020-02-19 22:53:09 +0100

    19.02.

---
+++ CycleCounter/Model/Parameters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycleCounter.Model
+{
+    class Parameters
+    {
+        public int ClockRate { get; set; }
+        public string Register1 { get; set; }
+        public string Register2 { get; set; }
+        public string Register3 { get; set; }
+    }
+}
