346e46c496d268c7e5e0de371067e36c335eb7e0
Michal Barth <u17.401@pslib.cloud>
2020-02-26 22:55:34 +0100

    26.2.

--- CycleCounter/ViewModels/MainViewModel.cs
+++ CycleCounter/ViewModels/MainViewModel.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CycleCounter.ViewModels
 {
-    class MainViewModel
+    class MainViewModel : INotifyPropertyChanged
     {
+        private int _cykly;
+        private int _cas;
+        private readonly string[] textyENG = { "1. Decide wether to input:", "Cycles:", "time/cycle rate:", "time/sec:", "CycleRate", "Click the GO Button", "GO button", "Save source code", "Copy/Paste:" };
+        public RelayCommand ToCZ { get; set; }
+        public int Cykly { get => _cykly; set { _cykly = value; NotifyPropertyChanged(); } }
+        public int Cas { get => _cas; set { _cas = value; NotifyPropertyChanged(); } }
+        public ParametrizedRelayCommand Play { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
